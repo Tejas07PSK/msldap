943e722159a536ec6ec8e79a561f10c0393d49d6
Palash Sarkar <palashsarkar0007@gmail.com>
2020-01-26 22:27:38 +0530

    Adding spring-boot application starter class !!

---
+++ src/main/java/org/gadstn/msldap/MsladpApplication.java
@@ -0,0 +1,13 @@
+package org.gadstn.msldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsladpApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run( MsladpApplication.class, args );
+    }
+
+}
