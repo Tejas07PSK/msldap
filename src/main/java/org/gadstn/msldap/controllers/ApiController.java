c692a565952d4befe99bdcfa5af2b95a24940eee
Palash Sarkar <palashsarkar0007@gmail.com>
2020-01-26 22:35:37 +0530

    Adding micro-service api controller class !!

---
+++ src/main/java/org/gadstn/msldap/controllers/ApiController.java
@@ -0,0 +1,32 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 26 Jan, 2020, 5:10 PM,
+        @File-Name : ApiController.java
+
+ */
+
+package org.gadstn.msldap.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.gadstn.msldap.services.UserService;
+import java.util.Map;
+
+@RestController
+public class ApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping( value = "/ldap/auth/get", method = RequestMethod.POST )
+    public String auth( @RequestBody Map < String, String > req ) throws Exception {
+
+        return userService.authenticate( req.get( "soeid" ), req.get( "pass" ) );
+
+    }
+
+}
