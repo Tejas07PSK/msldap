5e8f6a61a8679f4b7ce5b5b295b561081183fa05
Palash Sarkar <palashsarkar0007@gmail.com>
2020-01-26 22:52:35 +0530

    Adding default spring-test class !!

---
+++ src/test/java/org/gadstn/msladp/MsladpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gadstn.msladp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsladpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
