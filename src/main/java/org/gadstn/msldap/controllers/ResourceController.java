1d16fe3b2e0e66b08bf658864b8668b08c85d0f8
Palash Sarkar <palashsarkar0007@gmail.com>
2020-01-26 22:41:19 +0530

    Adding html static pages resource controller class !!

---
+++ src/main/java/org/gadstn/msldap/controllers/ResourceController.java
@@ -0,0 +1,20 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 26 Jan, 2020, 9:12 PM,
+        @File-Name : ResourceController.java
+
+ */
+
+package org.gadstn.msldap.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResourceController {
+
+    @GetMapping( "/login" )
+    public String returnLoginPage() { return ( "testlogin.html" ); }
+
+}
